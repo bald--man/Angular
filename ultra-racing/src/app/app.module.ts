d75c7d54a07bdd1300b6015a8ee14448a44a8bf6
Oleg Lipovoi <olipovoi@gmail.com>
2017-06-11 20:00:01 +0300

    Splitting to Two Components.

--- ultra-racing/src/app/app.module.ts
+++ ultra-racing/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CarPartsComponent } from './components/car-parts/car-parts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarPartsComponent
   ],
   imports: [
     BrowserModule
