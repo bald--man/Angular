f318901de7741d8fc984f637ee43c0e0acef5201
Oleg Lipovoi <olipovoi@gmail.com>
2017-06-24 22:29:20 +0300

    Search Detail

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SafePipe } from './utility/safe.pipe';
 import { VideoDetailComponent } from './video-detail/video-detail.component';
 import { VideoListComponent } from './video-list/video-list.component';
 import { SearchComponent } from './search/search.component';
+import { SearchDetailComponent } from './search-detail/search-detail.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { SearchComponent } from './search/search.component';
     VideoListComponent,
     SafePipe,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    SearchDetailComponent
   ],
   imports: [
     // ngx-bootstrap
