55e31fd7413867330f41804e7720dfc64ffbeaea
Oleg Lipovoi <olipovoi@gmail.com>
2017-06-26 16:26:45 +0300

    Passing Data to Components.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  description = 'A new app';
+  query: string;
+  private routeSub:any;
+
+  constructor(private route:ActivatedRoute){
+    this.routeSub = route.params.subscribe(params=>{
+      console.log(params)
+      this.query = params['q']
+    })
+  }
+
+  ngOnInit() { }
+
+  ngOnDestroy(){
+    this.routeSub.unsubscribe()
+  }
+
 }
